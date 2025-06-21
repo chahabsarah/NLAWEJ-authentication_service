195c818b983a8b39e292398e3ada8b3e05e22555
chahabsarah <sarra.chahab@esprit.tn>
2025-06-21 15:25:13 +0100

    Update InputValidationWithExpressValidator.js

--- MicroServicesBackend/authentication-service/src/validators/InputValidationWithExpressValidator.js
+++ MicroServicesBackend/authentication-service/src/validators/InputValidationWithExpressValidator.js
@@ -5,7 +5,7 @@ const inputValidatorExpress = [
     .notEmpty().withMessage('Fullname is required')
     .isLength({ min: 3 }).withMessage('Fullname must be at least 3 characters long')
     .matches(/^[A-Za-z\s]+$/).withMessage('Fullname must contain only letters and spaces')
-    .matches(/^[^\d!@#$%^&*(),.?":{}|<>\/]+(\s[^\d!@#$%^&*(),.?":{}|<>\/]+)*$/)
+    .matches(/^[A-Za-zÀ-ÿ\s'-]{3,}$/)
     .withMessage('Fullname cannot contain special characters or digits'),
 
   body('email')
