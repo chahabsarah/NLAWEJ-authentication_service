ece3e3661d1fd1bc99c4fc08338a37a404022055
chahabsarah <sarra.chahab@esprit.tn>
2025-06-21 15:13:50 +0100

    Update RoleMiddleware.js

--- MicroServicesBackend/authentication-service/src/middlewares/RoleMiddleware.js
+++ MicroServicesBackend/authentication-service/src/middlewares/RoleMiddleware.js
@@ -3,7 +3,7 @@ const checkAdminRole = (req, res, next) => {
     if (!req.user || !req.user.role) {
       return res.status(404).json({ message: "user role not found" });
     }
-    if (!req.user.role === "admin") {
+    if (req.user.role !== "admin") {
       return res.status(403).json({ message: "access denied !" });
     }
     next();
